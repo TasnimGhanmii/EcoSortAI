babfa56d88357c79a9038eb3507433b1afe22da4
tasnim <tasnimghanmi02@gmail.com>
2025-10-10 11:34:08 +0100

    init

---
+++ client/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import HelloWorld from './components/Login.vue'
+</script>
+
+<template>
+</template>
+
---
+++ client/src/components/Login.vue
@@ -0,0 +1,12 @@
+npm install -D unplugin-icons unplugin-vue-components unplugin-auto-import
+<script setup lang="ts">
+import { ref } from 'vue'
+
+
+
+</script>
+
+<template>
+  
+</template>
+
